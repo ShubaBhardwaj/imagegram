ea80b359b981a91d823b6bc05ea9798380fbac07
Shubham Bhardwaj <shubhambhardwaj@Shubhams-MacBook-Air.local>
2024-11-20 21:10:36 +0530

    Some little arrangement of file and folders for better cline code practice

--- src/Routers/apiRouter.js
+++ src/Routers/apiRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import v1Router from './v1Router.js';
+import v1Router from './v1/v1Router.js';
 
 
 router.use('/v1',v1Router);
--- src/Routers/v1/user.js
+++ src/Routers/v1/user.js
@@ -1,4 +1,4 @@
-import {getUserProfile} from "../Controller/userController.js";
+import {getUserProfile} from "../../Controller/userController.js";
 import express from "express";
 const router = express.Router();
 router.get("/profile", getUserProfile);
